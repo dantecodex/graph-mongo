716e29f71a0979e8e396bcc091bb4cc3f5b92667
dantecodex <anshulrajput237@gmail.com>
2025-03-03 15:21:22 +0530

    updated env file

--- src/db/connection.js
+++ src/db/connection.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/sales-analytics';
+const MONGODB_URI = process.env.MONGODB_URI
 
 const connectDB = async () => {
     try {
